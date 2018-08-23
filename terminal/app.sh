1a7d737becffa9369f2df8034fa46703384d27d9
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-23 10:43:27 +1000

    Fix passing option.

--- terminal/app.sh
+++ terminal/app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+URI_ROOT_PATH=${URI_ROOT_PATH:-/}
+
 exec butterfly.server.py --port=8080 --host=0.0.0.0 \
-    --uri-root-path "$URI_ROOT_PATH" --unsecure \
+    --uri-root-path="$URI_ROOT_PATH" --unsecure \
     --i-hereby-declare-i-dont-want-any-security-whatsoever
