19b6de7345ad66d2717efcdfec23c07c46833331
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-23 10:31:23 +1000

    Listen on all interfaces.

--- terminal/app.sh
+++ terminal/app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-exec butterfly.server.py --unsecure --port=8080 \
+exec butterfly.server.py --unsecure --port=8080 --host=0.0.0.0 \
     --i-hereby-declare-i-dont-want-any-security-whatsoever
