d5a9c773485e4cb929ac520adde85cb446881606
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-23 13:35:40 +1000

    Don't install as developer package.

--- terminal/app.sh
+++ terminal/app.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-URI_ROOT_PATH=${URI_ROOT_PATH:-/}
+URI_ROOT_PATH=${URI_ROOT_PATH:-}
 
 exec butterfly.server.py --port=8080 --host=0.0.0.0 \
-    --debug --uri-root-path="$URI_ROOT_PATH" --unsecure \
+    --uri-root-path="$URI_ROOT_PATH" --unsecure \
     --i-hereby-declare-i-dont-want-any-security-whatsoever
