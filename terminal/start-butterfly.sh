dd0ffe1ec8e78e62b48ef814c7f42b106e1b1976
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-23 16:41:37 +1000

    Link oc login.

--- terminal/start-butterfly.sh
+++ terminal/start-butterfly.sh
@@ -15,6 +15,12 @@ touch $HOME/.config/butterfly/butterfly.conf
 # shell to setup passwd file intercept using nss_wrapper, and enable
 # Python, before executing bash.
 
+env | grep KUBERNETES_ > /opt/app-root/etc/envvars
+
+oc config set-cluster local --server "https://$KUBERNETES_PORT_443_TCP_ADDR"
+oc config set-context me --cluster local --user "$JUPYTERHUB_USER"
+oc config use-context me
+
 exec butterfly.server.py --port=8080 --host=0.0.0.0 \
     --uri-root-path="$URI_ROOT_PATH" --unsecure \
     --i-hereby-declare-i-dont-want-any-security-whatsoever \
--- terminal/start-terminal.sh
+++ terminal/start-terminal.sh
@@ -2,4 +2,8 @@
 
 . /opt/app-root/etc/scl_enable
 
+set -a
+. /opt/app-root/etc/envvars
+set +a
+
 exec /bin/bash "$@"
