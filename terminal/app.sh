7ab4b91b22d26e0f058c9b4377ae866cc3a6b3d3
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-23 11:38:55 +1000

    Enable debug.

--- terminal/app.sh
+++ terminal/app.sh
@@ -5,5 +5,5 @@ set -x
 URI_ROOT_PATH=${URI_ROOT_PATH:-/}
 
 exec butterfly.server.py --port=8080 --host=0.0.0.0 \
-    --uri-root-path="$URI_ROOT_PATH" --unsecure \
+    --debug --uri-root-path="$URI_ROOT_PATH" --unsecure \
     --i-hereby-declare-i-dont-want-any-security-whatsoever
